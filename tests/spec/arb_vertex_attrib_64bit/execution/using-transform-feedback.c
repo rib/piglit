de5a262e553a852308fbb7861be83c7baa9175de
Alejandro Piñeiro <apinheiro@igalia.com>
2016-03-30 16:54:21 +0200

    attrib64: allow to specify the number of samples for using-transform-feedback
    Default is the previous fixed number, 5

--- tests/spec/arb_vertex_attrib_64bit/execution/using-transform-feedback.c
+++ tests/spec/arb_vertex_attrib_64bit/execution/using-transform-feedback.c
@@ -59,12 +59,13 @@ GLuint vbo;
 GLint inputAttrib;
 GLuint tbo;
 const char *varyings[] = { "outValue" };
-GLdouble data[] = { 1.00000000000000111022302462516E0,
-                    2.00000000000003730349362740526E0,
-                    3.00000000000002842170943040401E0,
-                    4.00000000000001065814103640150E0,
-                    5.00000000000047073456244106637E0 };
-GLdouble feedback[5];
+
+#define DEFAULT_NUM_SAMPLES 5
+unsigned num_samples = DEFAULT_NUM_SAMPLES;
+unsigned DATA_SIZE = 0;
+GLdouble LSB = 0.00000000010000111022302462516E0;
+GLdouble *data = NULL;
+GLdouble *feedback = NULL;
 
 static void
 init_shader()
@@ -81,19 +82,29 @@ init_shader()
 static void
 init_buffers()
 {
+        unsigned i;
+
+        DATA_SIZE = sizeof(GLdouble) * num_samples;
+        data = malloc(DATA_SIZE);
+        feedback = malloc(DATA_SIZE);
+
+        for (i = 0; i < num_samples; i++) {
+                data[i] = i + 1 + LSB;
+        }
+
         glGenVertexArrays(1, &vao);
         glBindVertexArray(vao);
 
         glGenBuffers(1, &vbo);
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, DATA_SIZE, data, GL_STATIC_DRAW);
 
         glEnableVertexAttribArray(inputAttrib);
         glVertexAttribLPointer(inputAttrib, 1, GL_DOUBLE, 0, 0);
 
         glGenBuffers(1, &tbo);
         glBindBuffer(GL_ARRAY_BUFFER, tbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(data), NULL, GL_STATIC_READ);
+        glBufferData(GL_ARRAY_BUFFER, DATA_SIZE, NULL, GL_STATIC_READ);
 }
 
 static void
@@ -104,7 +115,7 @@ render()
         glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, tbo);
 
         glBeginTransformFeedback(GL_POINTS);
-        glDrawArrays(GL_POINTS, 0, 5);
+        glDrawArrays(GL_POINTS, 0, num_samples);
         glEndTransformFeedback();
 
         glDisable(GL_RASTERIZER_DISCARD);
@@ -122,6 +133,12 @@ clean()
         glDeleteBuffers(1, &vbo);
 
         glDeleteVertexArrays(1, &vao);
+
+        if (data != NULL)
+                free(data);
+
+        if (feedback != NULL)
+                free(feedback);
 }
 
 static const char *double_to_hex(double d)
@@ -143,10 +160,10 @@ fetch_results()
         GLboolean result = GL_TRUE;
         int i;
 
-        glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0, sizeof(feedback), feedback);
+        glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0, DATA_SIZE, feedback);
 
-        for (i = 0; i < 5; i++) {
-                printf("%i - Original = %.14g[%s] Fetched = %.14g[%s] \n", i,
+        for (i = 0; i < num_samples; i++) {
+                printf("%i - Original = %.14g[%s] Fetched = %.14g[%s]\n", i + 1,
                        data[i], double_to_hex(data[i]),
                        feedback[i], double_to_hex(feedback[i]));
                 result = result && (data[i] == feedback[i]);
@@ -161,6 +178,26 @@ piglit_display(void)
         return PIGLIT_FAIL;
 }
 
+static void
+parse_args(int argc, char **argv)
+{
+        int num;
+
+        printf( "arb_vertex_attrib_64bit-using-transform-feedback <num_samples>\n");
+        printf( "\t<num_samples> is optional\n");
+        if (argc >= 2) {
+                num = atoi(argv[1]);
+                if (num > 0) {
+                        num_samples = num;
+                } else {
+                        fprintf(stderr, "Wrong value for samples: %s\n", argv[1]);
+                        num_samples = DEFAULT_NUM_SAMPLES;
+                }
+        }
+
+        printf( "Using %i samples\n", num_samples);
+}
+
 void piglit_init(int argc, char **argv)
 {
         GLboolean ok = GL_TRUE;
@@ -169,6 +206,8 @@ void piglit_init(int argc, char **argv)
         piglit_require_extension("GL_ARB_transform_feedback3");
         piglit_require_extension("GL_ARB_vertex_attrib_64bit");
         piglit_require_extension("GL_ARB_gpu_shader_fp64");
+
+        parse_args(argc, argv);
 
         init_shader();
         ok = ok && piglit_link_check_status(prog);
