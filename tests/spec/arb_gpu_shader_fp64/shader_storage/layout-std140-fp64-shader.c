6eab4fb0c93238917fe2404f0e80309db7ee7ba1
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-03-31 13:16:35 +0200

    Remove USE_DOUBLE defintions
    From now on, always with doubles

--- tests/spec/arb_gpu_shader_fp64/shader_storage/layout-std140-fp64-shader.c
+++ tests/spec/arb_gpu_shader_fp64/shader_storage/layout-std140-fp64-shader.c
@@ -47,34 +47,20 @@ PIGLIT_GL_TEST_CONFIG_END
 #define TOLERANCE 1e-5
 #define DIFFER(a,b) ((a > b ? a - b : b - a) > TOLERANCE)
 
-#define USE_DOUBLE
-
 static const char vs_code[] =
 	"#version 150\n"
 	"#extension GL_ARB_shader_storage_buffer_object : require\n"
         "#extension GL_ARB_gpu_shader_fp64 : require\n"
         "#extension GL_ARB_vertex_attrib_64bit : require\n"
 	"\n"
-	"layout(std140, row_major, binding=2) buffer ssbo {\n"
-#ifdef USE_DOUBLE
+	"layout(std430, row_major, binding=2) buffer ssbo {\n"
         "       double u[" XSTR(SSBO_SIZE) "/4];\n"
-#else
-        "       float u[" XSTR(SSBO_SIZE) "/4];\n"
-#endif
 	"};\n"
         "in vec4 vertex;\n"
-#ifdef USE_DOUBLE
 	"in double value;\n"
-#else
-	"in float value;\n"
-#endif
 	"void main() {\n"
 	"	gl_Position = vec4(vertex);\n"
-#ifdef USE_DOUBLE
 	"       u[0] = value;\n"
-#else
-	"       u[] = value;\n"
-#endif
         "}\n";
 
 static const char fs_source[] =
@@ -89,17 +75,9 @@ static const char fs_source[] =
 
 GLuint prog;
 
-#ifdef USE_DOUBLE
 double ssbo_values[SSBO_SIZE] = { 0.0 };
-#else
-float ssbo_values[SSBO_SIZE] = { 0.0 };
-#endif
 
-/* #ifdef USE_DOUBLE */
-/* static const double verts[] = { */
-/* #else */
 static const float verts[] = {
-/* #endif */
   6.776277770192E-21, 0.0, 0.0, 0.0,
   0.0, 0.0, 0.0, 0.0,
 };
@@ -125,13 +103,8 @@ piglit_init(int argc, char **argv)
 	glGenBuffers(1, &buffer);
 	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, buffer);
 
-#ifdef USE_DOUBLE
 	glBufferData(GL_SHADER_STORAGE_BUFFER, SSBO_SIZE*sizeof(double),
 				&ssbo_values[0], GL_DYNAMIC_DRAW);
-#else
-	glBufferData(GL_SHADER_STORAGE_BUFFER, SSBO_SIZE*sizeof(float),
-				&ssbo_values[0], GL_DYNAMIC_DRAW);
-#endif
 
         glViewport(0, 0, piglit_width, piglit_height);
 
@@ -177,41 +150,25 @@ enum piglit_result piglit_display(void)
         glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
   GLint attrib_location = glGetAttribLocation(prog, "value");
-  double v=3.50000000000727684579260312603E02; //0x4075E00000003202
-  glVertexAttribL1d(attrib_location, v);
+  double v1=3.50000000000727684579260312603E02; //0x4075E00000003202
+  double v2=5.00000000000047073456244106637E00; //0x4014000000000212
+  glVertexAttribL1d(attrib_location, v1);
 
         attrib_location = glGetAttribLocation(prog, "vertex");
 
-/* #ifdef USE_DOUBLE */
-/*         glVertexAttribLPointer(attrib_location, 4, GL_DOUBLE, 0, (const GLvoid *)0); */
-/* #else */
         glVertexAttribPointer(attrib_location, 4, GL_FLOAT, GL_FALSE, 0, (const GLvoid  *)0);
-/* #endif */
         glEnableVertexAttribArray(attrib_location);
         glDrawArrays(GL_LINES, 0, 2);
 
-#ifdef USE_DOUBLE
         double *map = (double *) glMapBuffer(GL_SHADER_STORAGE_BUFFER,  GL_READ_ONLY);
-#else
-        float *map = (float *) glMapBuffer(GL_SHADER_STORAGE_BUFFER,  GL_READ_ONLY);
-#endif
         int i;
 	for (i = 0; i < SSBO_SIZE; i+=4) {
-#ifdef USE_DOUBLE
           printf("read[%d] = (%.14g [%s],  %.14g [%s],  %.14g [%s],  %.14g [%s])\n",
                  i,
                  map[i], double_to_hex(map[i]),
                  map[i+1], double_to_hex(map[i+1]),
                  map[i+2], double_to_hex(map[i+2]),
                  map[i+3], double_to_hex(map[i+3]));
-#else
-          printf("read[%d] = (%.14g [%s],  %.14g [%s],  %.14g [%s],  %.14g [%s])\n",
-                 i,
-                 map[i], float_to_hex(map[i]),
-                 map[i+1], float_to_hex(map[i+1]),
-                 map[i+2], float_to_hex(map[i+2]),
-                 map[i+3], float_to_hex(map[i+3]));
-#endif
         }
 
   return PIGLIT_PASS;
